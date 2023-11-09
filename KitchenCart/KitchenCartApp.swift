ec28fdf5611a4f857fd4a1619b9c948089b8708a
Suyash Vashishtha <suyashvashishtha@gmail.com>
2023-11-10 00:30:06 +0530

    Initial Commit

---
+++ KitchenCart/KitchenCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenCartApp.swift
+//  KitchenCart
+//
+//  Created by Suyash Vashishtha on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct KitchenCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
